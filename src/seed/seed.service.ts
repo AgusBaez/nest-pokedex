3cc9bf3e5d8d036e3efdf9f091e6da01b5d8bcb0
Agus Baez <agustinbaezignacio@gmail.com>
2024-05-06 15:58:01 -0300

    custom provider

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,31 +1,34 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import axios, { AxiosInstance } from 'axios';
+// import axios, { AxiosInstance } from 'axios';
 import { Model } from 'mongoose';
 import { PokeResponse } from './inferfaces/poke-response.interfaces';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
-  // * adaptador de proovedores
-  protected readonly axios: AxiosInstance = axios;
+  // * REEMPLAZADO POR: adaptador de proovedores
+  //protected readonly axios: AxiosInstance = axios;
 
   constructor(
     @InjectModel(Pokemon.name)
     private readonly PokemonModule: Model<Pokemon>,
+
+    private readonly HtttpAd: AxiosAdapter,
   ) {}
 
   async executeSeed() {
     await this.PokemonModule.deleteMany(); // delete* from pokemon
 
     // * despus del get lo que esta en <_> es para especificar como tiene que ser la data que me llega de la respuesta http, tambien me permite usar las propiedades de la interface, interactuando mejor con los tipos de datos que quiero obtener
-    const { data } = await this.axios.get<PokeResponse>(
-      'https://pokeapi.co/api/v2/pokemon?limit=5',
+    const res = await this.HtttpAd.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=50',
     );
 
     const pokemonToInsert: { name: string; no: number }[] = [];
 
-    data.results.forEach(async ({ name, url }) => {
+    res.results.forEach(async ({ name, url }) => {
       const segments = url.split('/');
       const no = +segments[segments.length - 2];
 
